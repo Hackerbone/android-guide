a4acc5a81cc26cd907b5e05c3f4fa19e20b2d69f
Akshaj Vidyarthy <akshajvidyarthy03@gmail.com>
2025-04-16 11:11:16 +0530

    popup menu

---
+++ src/app/docs/popup_menu_db/page.js
@@ -0,0 +1,25 @@
+"use client";
+import styles from "@/app/page.module.css";
+import data from "./snippets.json";
+import CodeRenderer from "@/components/CodeRenderer";
+
+export default function Home() {
+  return (
+    <div className={styles.content}>
+      <h1>Popup Menu Database (Kotlin & SQLite)</h1>
+      <p>
+        This is a simple Popup Menu Database application built using Kotlin, Android
+        Studio, and SQLite for local storage. Below are the key code snippets
+        used in the implementation.
+      </p>
+
+      {data.snippets.map((snippet) => (
+        <CodeRenderer
+          key={snippet.codeblockTitle}
+          code={snippet.code}
+          title={snippet.codeblockTitle}
+        />
+      ))}
+    </div>
+  );
+}
